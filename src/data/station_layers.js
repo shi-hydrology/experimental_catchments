ecb8be1377fae84cf967d46b369695ea17647ea4
svet04ka <swechka06@gmail.com>
2021-09-30 18:05:23 +0300

    reworked icons

--- src/data/station_layers.js
+++ src/data/station_layers.js
@@ -1,16 +1,63 @@
-import {meteoIcon, wellIcon, prGaugeIcon, prGGI} from './type_icons.js'
+import {meteoIcon, wellIcon, prGaugeIcon, prGGI, snowIcon, soil_tempIcon, surf_elevIcon, thawIcon, evapIcon} from './type_icons.js'
 
-var station_layers=[{'type_name':'meteo',
-                     'rus_name':'Метеостанции',
-                     'type_icon': meteoIcon},
-                    {'type_name':'precipitation_gauge',
-                     'rus_name':'Осадкомеры',
-                     'type_icon': prGaugeIcon},
-                    {'type_name':'well',
-                     'rus_name':'Скважины',
-                     'type_icon': wellIcon},
-                    {'type_name':'kotiki_ggi',
-                     'rus_name':'Котики',
-                     'type_icon': prGGI},
+var station_layers = [{
+        'type_name': 'evaporation',
+        'rus_name': 'Испарение',
+        'type_icon': evapIcon
+    },
+    {
+        'type_name': 'swamp_water',
+        'rus_name': 'Болотные колодцы',
+        'type_icon': wellIcon
+    },
+    {
+        'type_name': 'groundwater',
+        'rus_name': 'Подземные воды',
+        'type_icon': wellIcon
+    },
+    {
+        'type_name': 'surf_elev',
+        'rus_name': 'surf_elev',
+        'type_icon': surf_elevIcon
+    },
+    {
+        'type_name': 'soil_temp',
+        'rus_name': 'soil_temp',
+        'type_icon': soil_tempIcon
+    },
+    {
+        'type_name': 'runoff',
+        'rus_name': 'Расходы',
+        'type_icon': prGGI
+    },
+    {
+        'type_name': 'precipitation',
+        'rus_name': 'Осадкомеры',
+        'type_icon': prGaugeIcon
+    },
+    {
+        'type_name': 'meteo',
+        'rus_name': 'Метеостанции',
+        'type_icon': meteoIcon
+    },
+    {
+        'type_name': 'thaw',
+        'rus_name': 'Thaw',
+        'type_icon': thawIcon
+    },
+    {
+        'type_name': 'snow',
+        'rus_name': 'Снегомеры',
+        'type_icon': snowIcon
+    },
+    {
+        'type_name': 'kotiki_ggi',
+        'rus_name': 'Котики',
+        'type_icon': prGGI
+    },
 ]
+
+
+
+
 export default station_layers
--- src/data/type_icons.js
+++ src/data/type_icons.js
@@ -1,8 +1,13 @@
 import * as L from 'leaflet';
-import meteo_icon from '../images/meteo.png';
-import well_icon from '../images/well.png';
-import pr_gauge_icon from '../images/rainfall.png';
-import ggi_icon from '../images/ggi_cat_icon.png';
+import meteo_icon from '../images/icons/meteo.png';
+import well_icon from '../images/icons/well.png';
+import pr_gauge_icon from '../images/icons/rainfall.png';
+import ggi_icon from '../images/icons/ggi_cat_icon.png';
+import snow_icon from '../images/icons/snow.ico';
+import soil_temp_icon from '../images/icons/soil_temp.png';
+import surf_elev_icon from '../images/icons/surf_elev.png';
+import thaw_icon from '../images/icons/thaw.png';
+import evap_icon from '../images/icons/evap.png';
 
 const meteoIcon = L.icon({
     iconUrl: meteo_icon,
@@ -32,4 +37,39 @@ const prGGI = L.icon({
     popupAnchor: [0, 0],
 });
 
-export {meteoIcon, wellIcon, prGaugeIcon, prGGI}
+const snowIcon = L.icon({
+    iconUrl: snow_icon,
+    iconSize: [45, 45],
+    iconAnchor: [45, 22],
+    popupAnchor: [0, 0],
+});
+
+const soil_tempIcon = L.icon({
+    iconUrl: soil_temp_icon,
+    iconSize: [45, 45],
+    iconAnchor: [45, 22],
+    popupAnchor: [0, 0],
+});
+
+const surf_elevIcon = L.icon({
+    iconUrl: surf_elev_icon,
+    iconSize: [45, 45],
+    iconAnchor: [45, 22],
+    popupAnchor: [0, 0],
+});
+
+const thawIcon = L.icon({
+    iconUrl: thaw_icon,
+    iconSize: [45, 45],
+    iconAnchor: [45, 22],
+    popupAnchor: [0, 0],
+});
+
+const evapIcon = L.icon({
+    iconUrl: evap_icon,
+    iconSize: [45, 45],
+    iconAnchor: [45, 22],
+    popupAnchor: [0, 0],
+});
+
+export {meteoIcon, wellIcon, prGaugeIcon, prGGI, snowIcon, soil_tempIcon, surf_elevIcon, thawIcon, evapIcon}
